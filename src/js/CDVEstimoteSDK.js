08259f34426e05b84e58d40ad9ec1d4ad2c4aa3e
thomas-alrek <thomas@apility.no>
2017-06-12 16:13:02 +0200

    Implement new funcs

--- src/js/CDVEstimoteSDK.js
+++ src/js/CDVEstimoteSDK.js
@@ -1,5 +1,14 @@
 const exec = cordova.require('cordova/exec')
 
+const setupAppID = (appId, appToken) => {
+  return new Promise((resolve, reject) => {
+    if (!appId || !appToken) {
+      reject(false)
+    }
+    exec(resolve, reject, 'CDVEstimoteSDK', 'setupAppID', [appId, appToken])
+  })
+}
+
 const requestAlwaysAuthorization = () => {
   return new Promise((resolve, reject) => {
     exec(resolve, reject, 'CDVEstimoteSDK', 'requestAlwaysAuthorization')
@@ -12,7 +21,22 @@ const requestWhenInUseAuthorization = () => {
   })
 }
 
+const reqisterForTelemetry = () => {
+  return new Promise((resolve, reject) => {
+    exec(resolve, reject, 'CDVEstimoteSDK', 'reqisterForTelemetry')
+  })
+}
+
+const startRangingBeacons = () => {
+  return new Promise((resolve, reject) => {
+    exec(resolve, reject, 'CDVEstimoteSDK', 'startRangingBeacons')
+  })
+}
+
 module.exports = {
+  setupAppID,
   requestAlwaysAuthorization,
-  requestWhenInUseAuthorization
+  requestWhenInUseAuthorization,
+  reqisterForTelemetry,
+  startRangingBeacons
 }
